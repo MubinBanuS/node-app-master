90eb3d2f4501f3ed5b520edf318b2fd2b301f140
Mubin S <mubins@softura.com>
2021-12-12 19:09:29 +0530

    code clean up

--- routes/employeeroutes.js
+++ routes/employeeroutes.js
@@ -1,111 +1,95 @@
-var sequelize=require('../orm/connection');
-const passport=require('passport')
-var express=require("express")
+var sequelize = require('../orm/connection');
+const passport = require('passport')
+var express = require("express")
 var route = express.Router();
-var model=require('../orm/model/skillmap');
+var model = require('../orm/model/index');
 
-route.get("/:employeeid",async function(request,response){
-try{
-   const employees = await model.employee.findOne({where:{employee_id:request.params.employeeid }})
-   let result = employees.dataValues
-   
-   if(result!=null) 
-      {
-         response.json(
-            {
-               name:result.name
-            }
-         )
-      }
-      else
-           response.status(401).send("Failed")
-}
-catch(e)
-{
-   console.log(e)
-        response.status(500)
-}
 
-})
+/**
+ * Gets manager grid data
+ */
 
-route.get("/manager/:name",passport.authenticate('jwt',{session:false}),async function(request,response){
-   try{
+route.get("/manager/:name", passport.authenticate('jwt', { session: false }), async function (request, response) {
+   try {
       const employees = await model.skillmap.findAll({
          group: ['employee_id'],
          attributes: ['employee_id'],
          include: [{
             model: model.employee,
-            attributes: ['name', 'experience','manager'],
+            attributes: ['name', 'experience', 'manager'],
             required: true,
-            where:{manager:request.params.name,lockstatus:'not_requested'}
-           }
-           ,{
-              model:model.skill,
-              attributes: [[sequelize.fn('GROUP_CONCAT', sequelize.col('skill.name')), 'skills']],
-              require:true
-           }
+            where: { manager: request.params.name, lockstatus: 'not_requested' }
+         }
+            , {
+            model: model.skill,
+            attributes: [[sequelize.fn('GROUP_CONCAT', sequelize.col('skill.name')), 'skills']],
+            require: true
+         }
          ]
       })
-      let managers=[];
+      let managers = [];
       employees.map(employee => {
-         let manager={
-            EmployeeId:employee.dataValues.employee_id,
-            Name:employee.dataValues.employee.name,
-            Skills:employee.dataValues.skill.dataValues.skills,
-            Experience:employee.dataValues.employee.experience,
-            Manager:employee.dataValues.employee.manager
+         let manager = {
+            EmployeeId: employee.dataValues.employee_id,
+            Name: employee.dataValues.employee.name,
+            Skills: employee.dataValues.skill.dataValues.skills,
+            Experience: employee.dataValues.employee.experience,
+            Manager: employee.dataValues.employee.manager
          }
          managers.push(manager)
       });
-      if(managers.length>0) 
-      {
+      if (managers.length > 0) {
          response.json(managers)
       }
       else
          response.status(401).send("Failed")
    }
-   catch(e)
-   {
+   catch (e) {
       console.log(e)
-           response.status(500)
+      response.status(500)
    }
 })
 
-
-route.post("/softlock",passport.authenticate('jwt',{session:false}),async function(request,response){
-      try{
-       
-         let ans={ 
-               employee_id: request.body.employee_id, 
-               manager: request.body.username,
-               status:'waiting',
-               requestmessage:request.body.requestmessage
-         }
-         const softlock = await model.softlock.create(ans);
-         let employee = await model.employee.findOne({ where: { employee_id: request.body.employee_id } })
-         employee.lockstatus = 'request_waiting';
-         await employee.save();
-         response.send("Requested successfully!")
+/**
+ * Adds soft lock request
+ */
+route.post("/softlock", passport.authenticate('jwt', { session: false }), async function (request, response) {
+   try {
+      //Adds soft lock request
+      let ans = {
+         employee_id: request.body.employee_id,
+         manager: request.body.username,
+         status: 'waiting',
+         requestmessage: request.body.requestmessage
       }
-      catch(e)
-      {
-         console.log(e)
-              response.status(500)
-      }
+      const softlock = await model.softlock.create(ans);
+      //updates employee status
+      let employee = await model.employee.findOne({ where: { employee_id: request.body.employee_id } })
+      employee.lockstatus = 'request_waiting';
+      await employee.save();
+      response.send("Requested successfully!")
+   }
+   catch (e) {
+      console.log(e)
+      response.status(500)
+   }
 })
 
-route.get("/wfm/:name",passport.authenticate('jwt',{session:false}),async function(request,response){
+/**
+ * Gets wfm grid data
+ */
+route.get("/wfm/:name", passport.authenticate('jwt', { session: false }), async function (request, response) {
    try {
       const manager_requests = await model.softlock.findAll({
          group: ['employee_id'],
-         attributes: ['employee_id','manager','reqdate','requestmessage'],
+         attributes: ['employee_id', 'manager', 'reqdate', 'requestmessage'],
          required: true,
          include: [{
-           model: model.employee,
-           attributes: ['wfm_manager'],
-           required: true,
-           where: { wfm_manager: request.params.name, lockstatus: 'request_waiting' }
-        }]
+            model: model.employee,
+            attributes: ['wfm_manager'],
+            required: true,
+            where: { wfm_manager: request.params.name, lockstatus: 'request_waiting' }
+         }]
       })
 
       let wfm_managers = [];
@@ -133,22 +117,28 @@ route.get("/wfm/:name",passport.authenticate('jwt',{session:false}),async functi
    }
 });
 
-route.post("/softlockstatus",passport.authenticate('jwt',{session:false}),async function(request,response){
-   
-   try{
+/**
+ * Updates soft lock request
+ */
+route.post("/softlockstatus", passport.authenticate('jwt', { session: false }), async function (request, response) {
+
+   try {
+      //updates softlock status
       let softlock = await model.softlock.findOne({ where: { employee_id: request.body.employee_id } })
       softlock.managerstatus = request.body.status;
       await softlock.save();
+
+      //updates employee status
       let employee = await model.employee.findOne({ where: { employee_id: request.body.employee_id } })
-      employee.lockstatus =  request.body.status==='accepted'? 'locked':'not_requested';
+      employee.lockstatus = request.body.status === 'accepted' ? 'locked' : 'not_requested';
       await employee.save();
-      response.send("Requested status updated successfully!") 
+      response.send("Requested status updated successfully!")
    }
    catch (e) {
       console.log(e)
       response.status(500)
    }
 });
-      
-      
-module.exports=  route
+
+
+module.exports = route
